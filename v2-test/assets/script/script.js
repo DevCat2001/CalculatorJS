3f961fb89366007cbf7d8e4b3bf25e0b8d2973b2
Andrea <andreagrieco01@gmail.com>
2021-09-22 20:59:26 +0200

    make a v2 version!

---
+++ v2-test/assets/script/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    let number_area = document.querySelector('.number-area');
+    let sign_area = document.querySelector('.sign-area');
+    number_area.innerHTML='';
+    sign_area.innerHTML='';
+    
+});
