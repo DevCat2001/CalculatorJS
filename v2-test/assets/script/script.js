9cc42305c9ebc50a3ed0812ed00a54419fc6e21b
Andrea <andreagrieco01@gmail.com>
2021-10-08 11:55:20 +0200

    bug fixes

--- v2-test/assets/script/script.js
+++ v2-test/assets/script/script.js
@@ -1,7 +1,13 @@
 document.addEventListener('DOMContentLoaded',()=>{
     let number_area = document.querySelector('.number-area');
     let sign_area = document.querySelector('.sign-area');
-    number_area.innerHTML='';
-    sign_area.innerHTML='';
+    number_area.innerHTML='bish lasaña';
+    sign_area.innerHTML='+';
     
+    let btn = document.querySelectorAll('button');
+    console.log(btn);
+    btn.forEach(element => {
+        console.log(element);
+        console.log(element.getAttribute('data-type'));
+    });
 });
